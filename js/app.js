8cbefb24ed640229eb997048662bcd2b86f1f9fe
karg315 <110070000+karg315@users.noreply.github.com>
2023-01-26 20:26:18 -0500

    Add files via upload
    V2.0

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function colorLibre(){
+    document.getElementById('body').style.backgroundColor='aliceblue';
+}
+
+function colorSloth(){
+    document.getElementById('body').style.backgroundColor='orange';
+}
+
+function colorGula(){
+    document.getElementById('body').style.backgroundColor='blue';
+}
+
+function colorDragon(){
+    document.getElementById('body').style.backgroundColor='red';
+}
+
+function colorCabra(){
+    document.getElementById('body').style.backgroundColor='purple';
+}
+
+function colorZorro(){
+    document.getElementById('body').style.backgroundColor='green';
+}
+
+function colorOrgullo(){
+    document.getElementById('body').style.backgroundColor='cyan';
+}
+
+function colorAngie(){
+    document.getElementById('body').style.backgroundColor='pink';
+}
